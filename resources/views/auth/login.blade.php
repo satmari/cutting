c6d6f28220085131ef3f75e845df88fca4f9ce16
satmari <satmariatila@gmail.com>
2025-07-25 10:04:10 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
 						
 
 						<div class="form-group">
-							<label class="col-md-4 control-label">Line / Modul / Cutting machine</label>
+							<label class="col-md-4 control-label">Username / Cutting machine</label>
 							<div class="col-md-6">
 								{{-- <input type="email" class="form-control" name="email" value="{{ old('email') }}"> --}}
 								<input type="text" class="form-control" name="email" value="{{ old('email') }}">
