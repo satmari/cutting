f6beb0f3f62955a9419d5ecfcbccd70b282a684e
satmari <satmariatila@gmail.com>
2025-11-03 13:41:12 +0100

    Update table_u_cons_search.blade.php

--- resources/views/pss/table_u_cons_search.blade.php
+++ resources/views/pss/table_u_cons_search.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Serch paspul cons table:
+                <div class="panel-heading">Search paspul cons table:
                     </div>
                 
                 @if (isset($msge))
